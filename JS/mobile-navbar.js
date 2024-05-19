26c7bebc810c380abe3cd7f84b01a8c360e8ed38
J0hD3v <johanfievet@orange.fr>
2024-05-19 17:25:57 +0200

    ajout carousel + debut media queries

---
+++ JS/mobile-navbar.js
@@ -0,0 +1,9 @@
+const btn_open_navbar_mobile = document.getElementById("btn_open_navbar");
+
+btn_open_navbar_mobile.addEventListener("click", function open_navbar() {
+    const navbar = document.createElement("div");
+    const navbar_btn_close = document.createElement("div");
+    const navbar_link1 = document.createElement("div");
+    const navbar_link2 = document.createElement("div");
+
+})
