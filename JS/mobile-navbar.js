babe35c1d5c6d88d1a28df57081e8089f7f1e3b3
J0hD3v <johanfievet@orange.fr>
2024-05-20 18:44:55 +0200

    maj media queries

--- JS/mobile-navbar.js
+++ JS/mobile-navbar.js
@@ -1,6 +1,8 @@
 const btn_open_navbar_mobile = document.getElementById("btn_open_navbar");
+const header = document.getElementsByTagName("header")[0];
+const navbar  =document.getElementsByClassName("header_nav")[0];
+const footer = document.getElementsByTagName("footer")[0];
 
 btn_open_navbar_mobile.addEventListener("click", function open_navbar() {
-    
-    
+    navbar.classList.toggle("mobile_nav_display");
 })
